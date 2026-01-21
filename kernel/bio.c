2956513fb5ab5f10cf2b9f37cc9c2ca218574d7c
tianci-song <songtianci179931@gmail.com>
2026-01-21 17:21:17 +0800

    redesign a much simplified scheme, pass manywrites test

--- kernel/bio.c
+++ kernel/bio.c
@@ -23,176 +23,29 @@
 #include "fs.h"
 #include "buf.h"
 
-//#define dbg
-
-/* implement a thread-safe hashtable for looking up the blockno in bcache */
-/* I use the double hash function combined with a linear probing method */
-/* The memory allocator in kernel is kalloc() which allocates a 4K page at once */
-/* It's hard to allocate specific small single space for each hashnode with this allocator */
-/* Thus, our buckets are not listed, but with a fixed array size */
-
-struct hashnode {
-	char key[BLKLEN];
-	int val;
-	struct spinlock lock;
-};
-
-uint64 hashfunc1(char key[], int tablesize) {
-	uint64 hash = 5381;
-	uint64 c;
-	while ((c = *key++)) {
-		hash = (hash << 5) + hash + c;	// hash = 33 * hash + c
-	}
-	return hash % tablesize;
-}
-
-uint64 hashfunc2(char key[], int tablesize) {
-	uint64 hash = 5381;
-	uint64 c;
-	while ((c = *key++)) {
-		hash = (hash << 5) + hash + c;	// hash = 33 * hash + c
-	}
-	return 1 + hash % (tablesize - 1);	// prime with hashfunc1
-}
-
 struct {
-	struct hashnode buckets[NBUCKETS];
-} hashtable;
-
-void init_hashtable() {
-	for(int i = 0; i < NBUCKETS; i++) {
-		struct hashnode* node = &hashtable.buckets[i];
-		initlock(&node->lock, "bucket");
-		acquire(&node->lock);
-		memset(node->key, 0, BLKLEN);	// empty string
-		node->val = -1;								// bucket is not owned
-		release(&node->lock);
-	}
-}
-
-// transfer the search result more easily
-struct stat {
-	int val;		// which cpu thread holds the key
-	int index;	// which bucket in that cpu thread's hashtable
-};
-
-/* core function: insert and delete all share this common lookup method */
-/* thus lookup() is very specified. lookup() takes 2 hash functions and */
-/* 1 linear probing to avoid the confliction as much as possible. */
-/* @param 'delete' is needed because when two key maps to the same */
-/* bucket with hashfunc1, one of them must be mapped to another bucket, */
-/* and once they are deleted, the first one can be deleted in the right */
-/* way while the other will be found non-existent, because it will be */
-/* detected by the hashfunc1 at first, whose bucket is empty after the */
-/* first one has been deleted, but it should be examined again with  */
-/* hashfunc2 and linear probing. */
-struct stat lookup_hashtable(char key[], int delete) {
-	if (key == nullptr) {
-		panic("lookup_hashtable(): key cannot be null\n");
-	}
-	if (strlen(key) > BLKLEN || strlen(key) <= 0) {
-		panic("lookup_hashtable(): key length is invalid\n");
-	}
-	struct stat s;
-	s.val = -1;
-	s.index = hashfunc1(key, NBUCKETS);
-	struct hashnode* node = &hashtable.buckets[s.index];
-	acquire(&node->lock);
-#ifdef dbg
-	printf("\n====== lookup_hashtable(): ======\n");
-	printf("  index: %d\n  dst: %s\n  src: %s\n  val: %d\n", s.index, node->key, key, node->val);
-	printf("=================================\n");
-#endif
-	if (node->val == -1 && !delete) {	// the key is not in the bucket and not lookup for delete
-		release(&node->lock);
-		return s;
-	}
-	if (node->val != -1 && strncmp(node->key, key, BLKLEN) == 0) {	// find the key
-		s.val = node->val;
-		release(&node->lock);
-		return s;
-	}
-	release(&node->lock);
-
-	// if an confliction happens, then use another hashfunc to find a new bucket
-	s.index = hashfunc2(key, NBUCKETS);
-	struct hashnode* node_2 = &hashtable.buckets[s.index];
-	acquire(&node_2->lock);
-#ifdef dbg
-	printf("\n====== lookup_hashtable(): ======\n");
-	printf("  index: %d\n  dst: %s\n  src: %s\n  val: %d\n", s.index, node_2->key, key, node_2->val);
-	printf("=================================\n");
-#endif
-	if (node_2->val == -1 && !delete) {	// the key not in the bucket and the lookup is not for delete
-		release(&node_2->lock);
-		return s;
-	}
-	if (node_2->val != -1 && strncmp(node_2->key, key, BLKLEN) == 0) {	// find the key
-		s.val = node_2->val;
-		release(&node_2->lock);
-		return s;
-	}
-	release(&node_2->lock);
-
-	// if still exist confliction, then a linear probe has to be done
-	int b_available = 0;	// store the first available bucket's index
-	int i = (s.index + 1) % NBUCKETS;
-	while (i != s.index) {
-		struct hashnode* node_i = &hashtable.buckets[i];
-		acquire(&node_i->lock);
-		// store the first available bucket if the key not exist at last
-		if (node_i->val == -1 && b_available == 0) {	
-			s.index = i;
-			b_available = 1;
-		}
-		if (strncmp(node_i->key, key, BLKLEN) == 0) {	// finally find the key
-			s.index = i;
-			s.val = node_i->val;
-			release(&node_i->lock);
-			return s;
-		}
-		i = (i + 1) % NBUCKETS;
-		release(&node_i->lock);
-	}
-	if (!b_available) {	// no buckets are available
-		panic("lookup_hashtable(): hash confliction cannot be handled\n");
-	}
-	return s;
-}
-
-void insert_hashtable(char key[], int val, int index) {
-	struct hashnode* node = &hashtable.buckets[index];
-	acquire(&node->lock);
-	safestrcpy(node->key, key, BLKLEN);
-	node->val = val;
-	release(&node->lock);
-}
-
-void delete_hashtable(char key[], int index) {
-	struct hashnode* node = &hashtable.buckets[index];
-	acquire(&node->lock);
-	memset(node->key, 0, BLKLEN);
-	node->val = -1;
-	release(&node->lock);
-}
-
-struct {
-  struct spinlock lock;
   struct buf buf[NBUF];
-} bcache[NCPU];
+	// if a thread wants to access a buffer, it needs to hold the lock of that buffer
+	// the index of that lock is a buf->blockno % NBUCKETS, like a hashfunc.
+	struct spinlock locks[NBUCKETS];	// avoid lock contention
+	struct spinlock biglock;	// to serialize some part of code
+} bcache;
 
 void
 binit(void)
 {
   struct buf *b;
 	for(int i = 0; i < NCPU; i++) {
-  	initlock(&bcache[i].lock, "bcache");
-		for(b = bcache[i].buf; b < bcache[i].buf+NBUF; b++){
+		for(b = bcache.buf; b < bcache.buf+NBUF; b++){
 			initsleeplock(&b->lock, "buffer");
+			b->refcnt = 0;
+			b->blockno = 0xffffffff;
 			b->ticks = ticks;
 		}
 	}
-	init_hashtable();
+	for(int i = 0; i < NBUCKETS; i++) {
+		initlock(&bcache.locks[i], "bcache.buckets");
+	}
 }
 
 // Look through buffer cache for block on device dev.
@@ -201,62 +54,45 @@ binit(void)
 static struct buf*
 bget(uint dev, uint blockno)
 {
-	char buf_blockno[BLKLEN] = {0};
-	snprintf(buf_blockno, sizeof(buf_blockno), "%d", blockno);
-
-#ifdef dbg
-	printf("\nbget() calling lookup:");
-#endif
-	struct stat s = lookup_hashtable(buf_blockno, 0);
-	int val = s.val;
-	if (val == -1) {	// not in the hashtable, so establish in the current cpu
-		push_off();
-		val = cpuid();	
-		pop_off();
-		insert_hashtable(buf_blockno, val, s.index);
-#ifdef dbg
-		printf("\nbget(): not in hashtable, assigned to cpu %d\n", val);
-#endif
-	}
-
-  struct buf *b;
-  acquire(&bcache[val].lock);
-  // Is the block already cached?
-  for(b = bcache[val].buf; b < bcache[val].buf + NBUF; b++){
-    if(b->dev == dev && b->blockno == blockno){
-      b->refcnt++;
+	// big lock is try for debugging, but accidentally resolve the problem of failing
+	// manywrites in usertests, though I still don't know why...
+	acquire(&bcache.biglock);		
+	int index = blockno % NBUCKETS;
+	acquire(&bcache.locks[index]);
+	// check if in the cached buffer
+	struct buf* b;
+	for(b = bcache.buf; b < bcache.buf + NBUF; b++){
+		if(b->dev == dev && b->blockno == blockno){
+			b->refcnt++;
 			b->ticks = ticks;
-      release(&bcache[val].lock);
-      acquiresleep(&b->lock);
-#ifdef dbg
-			printf("\nbget(): buf is in the cache\n  refcnt is %d\n  data is: %s\n", b->refcnt, b->data);
-#endif
-      return b;
-    }
-  }
-  // Not cached.
-  // Recycle the least recently used (LRU) unused buffer.
+			release(&bcache.locks[index]);
+			release(&bcache.biglock);
+			acquiresleep(&b->lock);
+			return b;
+		}
+	}
+	release(&bcache.biglock);
+
+	// If not cache, then recycle the least recently used (LRU) unused buffer.
 	uint minticks = 0xffffffff;
 	struct buf* bLRU = nullptr;
-  for(b = bcache[val].buf; b < bcache[val].buf + NBUF; b++){
-    if(b->refcnt == 0 && b->ticks < minticks) {
+	for(b = bcache.buf; b < bcache.buf + NBUF; b++) {
+		if(b->refcnt == 0 && b->ticks < minticks) {
 			bLRU = b;
 			minticks = b->ticks;
 		}
-  }
+	}
 	if (bLRU) {
 		bLRU->dev = dev;
 		bLRU->blockno = blockno;
 		bLRU->valid = 0;
 		bLRU->refcnt = 1;
-		release(&bcache[val].lock);
+		bLRU->ticks = ticks;
+		release(&bcache.locks[index]);
 		acquiresleep(&bLRU->lock);
-#ifdef dbg
-		printf("\nbget(): buf not in the cache\n  data is: %s\n", bLRU->data);
-#endif
 		return bLRU;
 	}
-  panic("bget: no buffers");
+	panic("no available buffer in the cache");
 }
 
 // Return a locked buf with the contents of the indicated block.
@@ -265,14 +101,6 @@ bread(uint dev, uint blockno)
 {
   struct buf *b;
   b = bget(dev, blockno);
-	
-	char buf_blockno[BLKLEN] = {0};
-	snprintf(buf_blockno, sizeof(buf_blockno), "%d", b->blockno);
-#ifdef dbg
-	printf("\nbread() calling lookup:");
-	struct stat s = lookup_hashtable(buf_blockno, 0);
-	printf("\nbread(): blockno %s read by cpu %d\n", buf_blockno, s.val);
-#endif
 
   if(!b->valid) {
     virtio_disk_rw(b, 0);
@@ -287,7 +115,8 @@ bwrite(struct buf *b)
 {
   if(!holdingsleep(&b->lock))
     panic("bwrite");
-  virtio_disk_rw(b, 1);
+
+	virtio_disk_rw(b, 1);
 }
 
 // Release a locked buffer.
@@ -297,61 +126,31 @@ brelse(struct buf *b)
 {
   if(!holdingsleep(&b->lock))
     panic("brelse");
-
   releasesleep(&b->lock);
 
-	char buf_blockno[BLKLEN] = {0};
-	snprintf(buf_blockno, sizeof(buf_blockno), "%d", b->blockno);
-#ifdef dbg
-	printf("\nbrelse: calling lookup:");
-#endif
-	struct stat s = lookup_hashtable(buf_blockno, 1);
-	int val = s.val;
-	if (val == -1) {
-		panic("data on buffer cache lost\n");
-	}
-
-  b->refcnt--;
+	int index = b->blockno % NBUCKETS;
+	acquire(&bcache.locks[index]);
+	b->refcnt--;
   if (b->refcnt == 0) {
 		b->dev = 0xffffffff;
 		b->blockno = 0xffffffff;
 		b->ticks = ticks;
-		delete_hashtable(buf_blockno, s.index);	
   }
+	release(&bcache.locks[index]);
 }
 
 void
 bpin(struct buf *b) {
-	char buf_blockno[BLKLEN] = {0};
-	snprintf(buf_blockno, sizeof(buf_blockno), "%d", b->blockno);
-#ifdef dbg
-	printf("\nbpin() calling lookup:");
-#endif
-	struct stat s = lookup_hashtable(buf_blockno, 0);
-	int val = s.val;
-	if (val == -1) {
-		panic("bpin():couldn't find the val due to hash confliction\n");
-	}
-  acquire(&bcache[val].lock);
+	int index = b->blockno % NBUCKETS;
+	acquire(&bcache.locks[index]);
   b->refcnt++;
-  release(&bcache[val].lock);
+	release(&bcache.locks[index]);
 }
 
 void
 bunpin(struct buf *b) {
-	char buf_blockno[BLKLEN] = {0};
-	snprintf(buf_blockno, sizeof(buf_blockno), "%d", b->blockno);
-#ifdef dbg
-	printf("\nbunpin() calling lookup:");
-#endif
-	struct stat s = lookup_hashtable(buf_blockno, 0);
-	int val = s.val;
-	if (val == -1) {
-		panic("bunpin():couldn't find the val due to hash confliction\n");
-	}
-  acquire(&bcache[val].lock);
+	int index = b->blockno % NBUCKETS;
+	acquire(&bcache.locks[index]);
   b->refcnt--;
-  release(&bcache[val].lock);
+	release(&bcache.locks[index]);
 }
-
-
--- kernel/param.h
+++ kernel/param.h
@@ -11,5 +11,5 @@
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
 #define MAXPATH      128   // maximum file path name
-#define NBUCKETS		 113	// size of buckets (prime number) in the hash table
+#define NBUCKETS		 13	// size of buckets (prime number) in the hash table
 #define BLKLEN			 8		// length of the string of blockno
