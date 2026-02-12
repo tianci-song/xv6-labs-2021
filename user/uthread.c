7168c8bade3db114cdf33c375ae7ab7fcee1b369
tianci-song <songtianci179931@gmail.com>
2026-02-12 15:31:37 +0800

    finish thread lab

--- user/uthread.c
+++ user/uthread.c
@@ -62,6 +62,9 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+		thread_switch(
+		 	(uint64)t->stack,
+			(uint64)current_thread->stack);
   } else
     next_thread = 0;
 }
@@ -76,6 +79,10 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+	uint64* ra = (uint64*)t->stack;
+	*ra = (uint64)func;
+	uint64* sp = (uint64*)(t->stack + 8);
+	*sp = (uint64)(t->stack + STACK_SIZE);
 }
 
 void 
